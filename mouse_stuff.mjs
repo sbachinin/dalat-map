fb3e4a22cd5cff35be41ec24bac7e3ca408bdf4f
sbachinin <ssbachinin@gmail.com>
2024-08-07 17:42:55 +0700

    close large photo when click outside

--- mouse_stuff.mjs
+++ mouse_stuff.mjs
@@ -4,32 +4,37 @@ const buildingHasDetails = featureMeta => {
     return featureMeta && (featureMeta.images || featureMeta.descr)
 }
 
-export const addMouseStuff = map => {
-    const detailsEl = document.querySelector('.details')
-    const imagesEl = detailsEl.querySelector('.images')
-    const fsPhotoEl = document.querySelector('.fullscreen-photo')
 
-    const showFrenchDetails = (details) => {
+const detailsEl = document.querySelector('.details')
+const imagesEl = detailsEl.querySelector('.images')
+const fsPhotoEl = document.querySelector('.fullscreen-photo')
 
-        if (details.images?.length) {
-            imagesEl.innerHTML = ''
-            for (const imgName of details.images) {
-                const imgEl = document.createElement('img')
-                imgEl.src = `./buildings_photos/${imgName}.jpg`
-                imagesEl.appendChild(imgEl)
-            }
+const showFrenchDetails = (details) => {
+
+    if (details.images?.length) {
+        imagesEl.innerHTML = ''
+        for (const imgName of details.images) {
+            const imgEl = document.createElement('img')
+            imgEl.src = `./buildings_photos/${imgName}.jpg`
+            imagesEl.appendChild(imgEl)
         }
-        
-        // show description
-
-        detailsEl.classList.remove('hidden')
     }
 
+    // show description
+
+    detailsEl.classList.remove('hidden')
+}
+
+
+
+export const addMouseStuff = map => {
+
+    // OPEN DETAILS OF A CLICKED BUILDING
     let selectedBuildingId = null
 
     map.on('click', (e) => {
-        
-        const maybeFrenchBuilding = map.queryRenderedFeatures(e.point).find(f => f.layer.id === 'French building')
+        const maybeFrenchBuilding = map.queryRenderedFeatures(e.point)
+            .find(f => f.layer.id === 'French building')
 
         const isAlreadySelected = maybeFrenchBuilding?.id === selectedBuildingId
         if (isAlreadySelected) return
@@ -46,24 +51,32 @@ export const addMouseStuff = map => {
         }
     })
 
-
+    // ADD & REMOVE CURSOR POINTER ON BUILDINGS WITH DETAILS
     map.on('mousemove', 'French building', (e) => {
-        if (e.features.length > 0) {
-            if (map.getZoom() < 15.5) return
-            if (buildingHasDetails(meta[e.features[0].id])) {
-                map.getCanvas().style.cursor = 'pointer'
-            }
-        }
-    });
-
+        if (e.features.length === 0) return
+        if (map.getZoom() < 15.5) return
+        if (!buildingHasDetails(meta[e.features[0].id])) return
+        map.getCanvas().style.cursor = 'pointer'
+    })
     map.on('mouseleave', 'French building', () => {
         map.getCanvas().style.cursor = ''
-    });
+    })
 
+    // OPEN LARGER PHOTO ON CLICK
     imagesEl.addEventListener('click', e => {
+        if (e.target.tagName !== "IMG") return
         const imgEl = document.createElement('img')
         imgEl.src = e.target.src
         fsPhotoEl.appendChild(imgEl)
         fsPhotoEl.classList.remove('hidden')
+    })
+
+    // CLOSE LARGE PHOTO WHEN CLICK OUTSIDE
+    fsPhotoEl.addEventListener('click', e => {
+        setTimeout(() => {
+            if (e.target.tagName === "IMG") return // OR SWITCH TO THE NEXT PHOTO?
+            fsPhotoEl.innerHTML = ''
+            fsPhotoEl.classList.add('hidden')
+        })
     })
 }
