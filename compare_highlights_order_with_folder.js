f2f109e269815410ca41179294f49d622112a379
sbachinin <ssbachinin@gmail.com>
2024-11-04 16:25:40 +0700

    add compare highlights

---
+++ compare_highlights_order_with_folder.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const highlights_images_list = require('./highlights_images_list.js');
+
+function compareArrayWithDirectory(dirPath, arrayToCompare) {
+    const filesInDirectory = fs.readdirSync(dirPath)
+        .map(file => path.basename(file))
+        .filter(filename => !filename.match('Zone.Identifier'));
+
+    const arraySet = new Set(arrayToCompare);
+    const directorySet = new Set(filesInDirectory);
+
+    const isEqual = arraySet.size === directorySet.size && [...arraySet].every(file => {
+        return directorySet.has(file)
+    });
+
+    if (isEqual) {
+        console.log("OK");
+    } else {
+        console.log("WARNING: The array does not match the directory contents.");
+    }
+}
+
+const directoryPath = '../dalat-map-images/originals';
+compareArrayWithDirectory(directoryPath, highlights_images_list);
