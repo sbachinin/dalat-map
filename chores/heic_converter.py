e081acb7f47ba7035955f7f0c6985113d1ea3cb7
sbachinin <ssbachinin@gmail.com>
2024-11-04 17:16:34 +0700

    better resize images with heic conversion

---
+++ chores/heic_converter.py
@@ -0,0 +1,22 @@
+# heic_converter.py
+
+import pyheif
+from PIL import Image
+
+def convert_heic_to_jpg(heic_path, jpg_path):
+    """Convert HEIC file to JPG format."""
+    # Read the HEIC file
+    heif_file = pyheif.read(heic_path)
+    
+    # Convert to a Pillow Image
+    image = Image.frombytes(
+        heif_file.mode, 
+        heif_file.size, 
+        heif_file.data, 
+        "raw", 
+        heif_file.mode, 
+        heif_file.stride,
+    )
+    
+    # Save the image as JPG
+    image.save(jpg_path, "JPEG", quality=100)
---
+++ chores/resize_images.py
@@ -0,0 +1,53 @@
+import os
+from PIL import Image
+from heic_converter import convert_heic_to_jpg
+
+# if not installed, pip install Pillow
+
+source_folder = '../dalat-map-images/originals'
+thumbs_folder = '../dalat-map-images/thumbs'
+large_folder = '../dalat-map-images/large'
+
+os.makedirs(thumbs_folder, exist_ok=True)
+
+for filename in os.listdir(thumbs_folder):
+    file_path = os.path.join(thumbs_folder, filename)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+
+for filename in os.listdir(large_folder):
+    file_path = os.path.join(large_folder, filename)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+
+for filename in os.listdir(source_folder):
+    
+    if filename.lower().endswith('.heic'):
+        heic_file_path = os.path.join(source_folder, filename)
+        filename_without_extension, _ = os.path.splitext(filename)
+        jpg_filename = filename_without_extension + '.jpg'
+        jpg_file_path = os.path.join(source_folder, jpg_filename)
+        convert_heic_to_jpg(heic_file_path, jpg_file_path)
+        filename = jpg_filename
+    
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+        img_path = os.path.join(source_folder, filename)
+        with Image.open(img_path) as img:
+            
+            # 1. thumb
+
+            width_percent = (215 / float(img.size[0]))
+            new_height = int((float(img.size[1]) * float(width_percent)))
+            img1 = img.resize((215, new_height), Image.LANCZOS)
+            optimized_img_path = os.path.join(thumbs_folder, filename)
+            img1.save(optimized_img_path, quality=95)
+
+            # 2. big
+
+            width_percent = (800 / float(img.size[0]))
+            new_height = int((float(img.size[1]) * float(width_percent)))
+            img2 = img.resize((800, new_height), Image.LANCZOS)
+            large_img_path = os.path.join(large_folder, filename)
+            img2.save(large_img_path, quality=95)
+
+print("Images have been resized")
