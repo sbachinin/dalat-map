bdff8723e2f79358879cc2806022f506589af614
sbachinin <ssbachinin@gmail.com>
2024-11-05 17:34:32 +0700

    Add #non-map element

--- js/panel.mjs
+++ js/panel.mjs
@@ -1,26 +1,28 @@
-/* export const create_element_from_Html = htmlString => {
-    if (typeof htmlString !== 'string') {
-        console.warn('create_element_from_Html expects an html string, instead got: ', htmlString)
-        return document.createElement('div')
-    }
+const do_with_transition = (fn) => {
+    panelEl.classList.remove('notransition')
+    setTimeout(fn)
+    setTimeout(() => {
+        panelEl.classList.add('notransition')
+    }, 250)
+}
 
-    const div = document.createElement('div')
-    div.innerHTML = htmlString.trim()
-    if (!div.firstElementChild) {
-        console.warn(`create_element_from_Html: failed to create an element from string: "${htmlString}"`)
-        return document.createElement('div')
-    }
-    return div.firstElementChild
-} */
-
-const el = document.querySelector(`#panel-expander`)
+const panelEl = document.querySelector(`#panel-expander`)
 
 export const panel = {
-    element: el,
+    element: panelEl,
     expand() {
-        el.classList.add('expanded')
+        do_with_transition(() => { panelEl.classList.add('expanded') })
     },
     collapse() {
-        el.classList.remove('expanded')
+        do_with_transition(() => { panelEl.classList.remove('expanded') })
     },
+    insertContent() {
+
+    }
 }
+
+document.addEventListener('click', function (event) {
+    if (!panelEl.contains(event.target)) {
+        panel.collapse()
+    }
+});
