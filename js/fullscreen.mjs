fb832b65aa8d7c7109d1ed65d3e28750722c307f
sbachinin <ssbachinin@gmail.com>
2024-11-08 17:49:52 +0700

    try hide button if fullscreen is not supported

--- js/fullscreen.mjs
+++ js/fullscreen.mjs
@@ -1,35 +1,43 @@
 const fullscreenBtn = document.getElementById('fullscreen-btn');
 
-  fullscreenBtn.addEventListener('click', function() {
-    if (document.fullscreenElement) {
-      exitFullscreen();
-    } else {
-      goFullscreen();
-    }
-  });
+if (
+    document.documentElement.requestFullscreen
+    || document.documentElement.mozRequestFullScreen
+    || document.documentElement.webkitRequestFullscreen
+    || document.documentElement.msRequestFullscreen
+) {
+    fullscreenBtn.addEventListener('click', function () {
+        if (document.fullscreenElement) {
+            exitFullscreen();
+        } else {
+            goFullscreen();
+        }
+    });
+    fullscreenBtn.classList.remove('hidden')
+}
 
-  // Enter fullscreen
-  function goFullscreen() {
+// Enter fullscreen
+function goFullscreen() {
     if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
+        document.documentElement.requestFullscreen();
     } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-      document.documentElement.mozRequestFullScreen();
+        document.documentElement.mozRequestFullScreen();
     } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, Opera
-      document.documentElement.webkitRequestFullscreen();
+        document.documentElement.webkitRequestFullscreen();
     } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-      document.documentElement.msRequestFullscreen();
+        document.documentElement.msRequestFullscreen();
     }
-  }
+}
 
-  // Exit fullscreen
-  function exitFullscreen() {
+// Exit fullscreen
+function exitFullscreen() {
     if (document.exitFullscreen) {
-      document.exitFullscreen();
+        document.exitFullscreen();
     } else if (document.mozCancelFullScreen) { // Firefox
-      document.mozCancelFullScreen();
+        document.mozCancelFullScreen();
     } else if (document.webkitExitFullscreen) { // Chrome, Safari, Opera
-      document.webkitExitFullscreen();
+        document.webkitExitFullscreen();
     } else if (document.msExitFullscreen) { // IE/Edge
-      document.msExitFullscreen();
+        document.msExitFullscreen();
     }
-  }
+}
