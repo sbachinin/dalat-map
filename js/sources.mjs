6eb2a59f62fb9d5a6d6b1472b25118fab33ae623
sbachinin <ssbachinin@gmail.com>
2024-11-08 17:18:43 +0700

    request tiles locally from within this repo (but gitignored)

--- js/sources.mjs
+++ js/sources.mjs
@@ -1,24 +1,14 @@
 import dalatBulkJSON from '../data/dalat-bulk-geometry.mjs'
 
 export const sources = {
-    "contours": {
-        "url": "https://api.maptiler.com/tiles/contours/tiles.json?key=9C8cs9qKiCW3wSWdeUrN",
-        "type": "vector"
-    },
-    "maptiler_planet": {
-        "url": "https://api.maptiler.com/tiles/v3/tiles.json?key=9C8cs9qKiCW3wSWdeUrN",
-        "type": "vector"
-    },
-    
+    // "contours": {
+    //     "url": "https://api.maptiler.com/tiles/contours/tiles.json?key=9C8cs9qKiCW3wSWdeUrN",
+    //     "type": "vector"
+    // },
     'dalat-tiles': {
         type: 'vector',
-        tiles: [`https://sbachinin.github.io/dalat-map-tiles/tiles/{z}/{x}/{y}.pbf`],
+        tiles: [`${window.location.origin}/dalat-map-tiles/tiles/{z}/{x}/{y}.pbf`],
         minzoom: 10,
-    },
-
-    "maptiler_attribution": {
-        "attribution": "<a href=\"https://www.maptiler.com/copyright/\" target=\"_blank\">&copy; MapTiler</a> <a href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\">&copy; OpenStreetMap contributors</a>",
-        "type": "vector"
     },
     "cityBulk": {
         "type": "geojson",
