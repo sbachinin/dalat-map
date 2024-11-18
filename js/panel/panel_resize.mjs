9ff3e8bc7e8501394f17c5e61d64103d46f166c3
sbachinin <ssbachinin@gmail.com>
2024-11-18 14:57:56 +0700

    add-remove notransition on resize

--- js/panel/panel_resize.mjs
+++ js/panel/panel_resize.mjs
@@ -1,6 +1,7 @@
 export const handle_resize = (panel) => {
 
     const onresize = () => {
+        panel.element.classList.add('notransition')
         panel.content.update()
         panel.element.scrollTop = 0
         panel.element.scrollLeft = 0
@@ -9,6 +10,7 @@ export const handle_resize = (panel) => {
         Promise.all([panel.is_expanded(), panel.full_size_promise])
             .then(([was_expanded, full_size]) => {
                 panel.set_size(was_expanded ? full_size : 0)
+                panel.element.classList.remove('notransition')
             })
     }
 
