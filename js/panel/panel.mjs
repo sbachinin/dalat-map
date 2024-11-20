0e9423333cfe3824f864549be859b027a732ccca
sbachinin <ssbachinin@gmail.com>
2024-11-20 19:35:51 +0700

    Add content fade/reveal transition

--- js/panel/panel.mjs
+++ js/panel/panel.mjs
@@ -1,6 +1,6 @@
 import { make_expandable_on_swipe } from './panel_swipe.mjs'
 import { get_css_var_num, set_css_num_var, debounce } from '../utils.mjs'
-import { get_panel_intrinsic_size, } from './panel_utils.mjs'
+import { get_panel_intrinsic_size, get_panel_el } from './panel_utils.mjs'
 import { handle_resize } from './panel_resize.mjs'
 
 const EXPAND_TRANSITION_DURATION = 350
@@ -13,7 +13,7 @@ const expand_button_el = document.querySelector(`#panel-expand-button`)
 const panel_expander_el = document.querySelector(`#panel-expander`)
 
 const update_expand_button = debounce(async () => {
-    const was_expanded = await panel.is_expanded()
+    const was_expanded = await panel.is_rather_expanded()
     expand_button_el.classList[was_expanded ? 'add' : 'remove']('inward')
 })
 
@@ -32,6 +32,10 @@ export const panel = {
         if (size !== undefined) {
             set_css_num_var('--panel-size', size, 'px')
             update_expand_button()
+            panel.is_rather_expanded().then(was_expanded => {
+                const method = was_expanded ? 'add' : 'remove'
+                panel.content.element.classList[method]('opaque')
+            })
         } else {
             // TODO remove console warning
             console.warn('no size passed to panel.set_size')
@@ -41,10 +45,10 @@ export const panel = {
         panel.set_size(await panel.full_size_promise)
     },
     async toggle() {
-        const was_expanded = await panel.is_expanded()
+        const was_expanded = await panel.is_rather_expanded()
         was_expanded ? panel.set_size(0) : panel.expand()
     },
-    async is_expanded() {
+    async is_rather_expanded() {
         const full_size = await panel.full_size_promise
         return get_css_var_num('--panel-size') > full_size / 2
     },
--- js/panel/panel_resize.mjs
+++ js/panel/panel_resize.mjs
@@ -7,7 +7,7 @@ export const handle_resize = (panel) => {
         panel.element.scrollLeft = 0
         panel.cache_full_size()
 
-        Promise.all([panel.is_expanded(), panel.full_size_promise])
+        Promise.all([panel.is_rather_expanded(), panel.full_size_promise])
             .then(([was_expanded, full_size]) => {
                 panel.set_size(was_expanded ? full_size : 0)
                 panel.element.classList.remove('notransition')
