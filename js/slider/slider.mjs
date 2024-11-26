8fa84597ddf43602ea7e32911555fbb75f28db41
sbachinin <ssbachinin@gmail.com>
2024-11-26 15:28:11 +0700

    re-append slides only if content_type has changed

--- js/slider/slider.mjs
+++ js/slider/slider.mjs
@@ -56,21 +56,31 @@ root_swiper_el.querySelector('.close-btn').addEventListener('click', () => {
 let swiper = null
 
 export const open_slider = ({ initial_index, max_index, get_slide, content_type }) => {
-    if (swiper && !swiper.destroyed && root_swiper_el.dataset.content_type === content_type) {
+    const content_has_changed = root_swiper_el.dataset.content_type !== content_type
+
+    if (swiper && !swiper.destroyed && !content_has_changed) {
         swiper.slideTo(initial_index)
         root_swiper_el.classList.add('visible')
         return
     }
-    
+
+    // here: no swiper alive
+
     root_swiper_el.setAttribute('data-content_type', content_type)
     const wr = document.querySelector('.swiper-wrapper')
 
-    do_n_times(max_index, i => {
-        const swiper_slide_el = document.createElement('div')
-        swiper_slide_el.className = 'swiper-slide'
-        swiper_slide_el.appendChild(get_slide(wrap(i, 0, max_index)))
-        wr.appendChild(swiper_slide_el)
-    })
+    if (content_has_changed) {
+        wr.innerHTML = ''
+        do_n_times(max_index, i => {
+            const swiper_slide_el = document.createElement('div')
+            swiper_slide_el.className = 'swiper-slide'
+            swiper_slide_el.appendChild(get_slide(wrap(i, 0, max_index)))
+            wr.appendChild(swiper_slide_el)
+        })
+
+    }
+
+    root_swiper_el.classList.add('visible') // important to do this before new Swiper, otherwise strange blinking on re-opening
 
     swiper = new Swiper('.swiper-container', {
         initialSlide: initial_index,
@@ -79,11 +89,12 @@ export const open_slider = ({ initial_index, max_index, get_slide, content_type 
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        }
     })
 
-    root_swiper_el.classList.add('visible')
-
-    return
 
     // img_el.onload = () => {
     //     insert_slide_and_try_activate_img(_wrap(initial_index - 1))
