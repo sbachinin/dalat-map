e1848429d27ac622a37a87fa72bc3fea7112e95b
sbachinin <ssbachinin@gmail.com>
2025-01-03 13:15:44 +0700

    Add some french meta

--- js/french_buildings_meta.mjs
+++ js/french_buildings_meta.mjs
@@ -1,19 +1,86 @@
 const meta =  {
-    "361692216": {
+    "17877171": {
         "images": [
-            "IMG_0279~2.jpg"
+            "IMG_0673.jpg",
+            "IMG_0790~4.jpg",
+            "IMG_0808.jpg"
         ]
     },
-    "361851888": {
-        "descr": "Built in 1935 . female students from wealthy families in Dalat and southern Vietnam were educated between 1940 and 1975. Queen Nam Phuong was reportedly a student of this school as well.\n        I entered the campus without a problem. The building stands next to an old pine grove."
+    "17929361": {
+        "images": [
+            "IMG_1302~4.jpg",
+            "IMG_1307~2.jpg"
+        ]
+    },
+    "263418649": {
+        "images": [
+            "IMG_0811~2.jpg"
+        ]
+    },
+    "308446691": {
+        "images": [
+            "IMG_1791.jpg"
+        ]
+    },
+    "361692216": {
+        "images": [
+            "IMG_0279~2.jpg",
+            "IMG_1563~3.jpg"
+        ]
+    },
+
+    "361851915": {
+        "descr": "Built in 1935 . female students from wealthy families in Dalat and southern Vietnam were educated between 1940 and 1975. Queen Nam Phuong was reportedly a student of this school as well.\n        I entered the campus without a problem. The building stands next to an old pine grove.",
+        "images": [
+            "IMG_0218~2.jpg",
+            "IMG_2095~2.jpg"
+        ]
+    },
+    "515193504": {
+        "images": [
+            "IMG_0398~3.jpg",
+            "IMG_0405~3.jpg"
+        ]
     },
     "515193507": {
         "images": [
+            "IMG_0567~3.jpg",
+            "IMG_0573~3.jpg",
             "IMG_0601~3.jpg"
         ]
     },
     "515288591": {
         "title": "National archives center"
+    },
+    "542725574": {
+        "images": [
+            "IMG_0145~3.jpg"
+        ]
+    },
+    "543357192": {
+        "images": [
+            "IMG_0275~2.jpg",
+            "IMG_0280.jpg",
+            "IMG_1172~3.jpg"
+        ]
+    },
+    "543357209": {
+        "images": [
+            "IMG_0587~2.jpg"
+        ]
+    },
+    "618049068": {
+        "images": [
+            "IMG_1339.jpg"
+        ]
+    },
+    "618049072": {
+        "images": [
+            "IMG_1227~2.jpg",
+            "IMG_1248.jpg",
+            "IMG_1255~2.jpg",
+            "IMG_1259.jpg"
+        ]
     },
     "777432545": {
         "images": [
@@ -30,7 +97,9 @@ const meta =  {
     },
     "777438288": {
         "images": [
-            "IMG_0102~2.JPG"
+            "IMG_0102~2.JPG",
+            "IMG_0745~5.jpg",
+            "IMG_0758~4.jpg"
         ]
     },
     "777438290": {
@@ -38,95 +107,394 @@ const meta =  {
             "IMG_0074~4.JPG"
         ]
     },
+    "777438291": {
+        "images": [
+            "IMG_0082~2.JPG"
+        ]
+    },
+    "777438297": {
+        "images": [
+            "IMG_0110~2.JPG",
+            "IMG_0708~4.jpg",
+            "IMG_0709~2.jpg"
+        ]
+    },
     "777438901": {
         "descr": "One of the 3 palaces of king Bao Dai. (All 3 in Dalat).\nThis on was built in 1933-1937.\nNow it's possibly used as an event space by some bosses of Lam Dong province.\nThe building is massive and rather clumsy and bureaucratic to my taste. One explanation of this is that Palace II was designed as headquarters of the French ruling apparatus in Indochina. (Though this fact comes from a dubious source in the web).\nHowever clumsiness is redeemed by the setting: a vast pine forest on the very top of the hill. A really tranquil and enigmatic place.\nThe palace is well maintained but still shows a bit of historical shabbiness which is nice. At the time of my visit the surrounding area was completely deserted.\nAccess seems to be unrestricted, I just walked through the entrance from the south. (Not from main street)."
     },
+    "777439875": {
+        "images": [
+            "IMG_0984.jpg"
+        ]
+    },
+    "777439882": {
+        "images": [
+            "IMG_0970~2.jpg"
+        ]
+    },
+    "777439883": {
+        "images": [
+            "IMG_1009~2.jpg"
+        ]
+    },
     "971545940": {
         "images": [
+            "IMG_0492~3.jpg",
+            "IMG_0518~2.jpg",
             "IMG_1431~4.jpg",
             "IMG_1438~2.jpg"
         ]
     },
+    "971549873": {
+        "images": [
+            "IMG_0941.jpg"
+        ]
+    },
     "971558874": {
         "images": [
+            "IMG_0594~2.jpg",
             "IMG_0596~3.jpg",
-            "IMG_0613.jpg"
+            "IMG_0613.jpg",
+            "IMG_0615~2.jpg"
+        ]
+    },
+    "971558882": {
+        "images": [
+            "IMG_1980.jpg",
+            "IMG_1999.jpg"
+        ]
+    },
+    "971558884": {
+        "images": [
+            "IMG_0531~3.jpg"
         ]
     },
     "971558885": {
         "images": [
-            "424.jpg",
-            "IMG_0550~2.jpg"
+            "IMG_0550~2.jpg",
+            "IMG_0561~2.jpg",
+            "IMG_0876.jpg",
+            "IMG_0885.jpg",
+            "IMG_2021~3.jpg"
         ]
     },
     "971563667": {
         "images": [
-            "IMG_0429~3.jpg"
+            "IMG_0397~2.jpg",
+            "IMG_0425~2.jpg",
+            "IMG_0429~3.jpg",
+            "IMG_0441~2.jpg"
+        ]
+    },
+    "971568004": {
+        "images": [
+            "IMG_0250~2.jpg"
+        ]
+    },
+    "971568007": {
+        "images": [
+            "IMG_0230~3.jpg"
         ]
     },
     "971568008": {
         "images": [
-            "16_1.jpg"
+            "IMG_1070.jpg",
+            "IMG_1468~5.jpg"
+        ]
+    },
+    "971572395": {
+        "images": [
+            "14_1.jpg",
+            "15_1~2.jpg",
+            "434.jpg",
+            "IMG_0489~3.jpg",
+            "IMG_1563~5.jpg",
+            "IMG_1571~4.jpg"
+        ]
+    },
+    "971577493": {
+        "images": [
+            "427_2.jpg",
+            "IMG_0469~2.jpg",
+            "IMG_0475~2.jpg"
         ]
     },
     "1243929950": {
         "images": [
             "IMG_0769~2.jpg",
-            "IMG_0800~2.jpg"
+            "IMG_0800~2.jpg",
+            "IMG_0830~2.jpg",
+            "IMG_2053.jpg",
+            "IMG_2065.jpg",
+            "IMG_2069.jpg"
+        ]
+    },
+    "1301437304": {
+        "images": [
+            "IMG_0723~6.jpg",
+            "IMG_2009~2.jpg"
+        ]
+    },
+    "1302653950": {
+        "images": [
+            "IMG_0314~3 Copy.JPG"
+        ]
+    },
+    "1302653951": {
+        "images": [
+            "IMG_0287~3.jpg"
+        ]
+    },
+    "1302653952": {
+        "images": [
+            "IMG_0255~2.jpg"
+        ]
+    },
+    "1302653954": {
+        "images": [
+            "IMG_0327~2.jpg"
+        ]
+    },
+    "1302812573": {
+        "images": [
+            "IMG_2103~4.jpg"
+        ]
+    },
+    "1302812580": {
+        "images": [
+            "IMG_2038~2.jpg"
+        ]
+    },
+    "1302812586": {
+        "images": [
+            "IMG_0894.jpg"
+        ]
+    },
+    "1303227312": {
+        "images": [
+            "IMG_1596.jpg"
+        ]
+    },
+    "1303227327": {
+        "images": [
+            "IMG_1750.jpg"
         ]
     },
     "1303825521": {
         "images": [
-            "IMG_0724~2.jpg"
+            "IMG_0724.jpg",
+            "IMG_0724~2.jpg",
+            "IMG_0733.jpg"
+        ]
+    },
+    "1303825522": {
+        "images": [
+            "IMG_0737.jpg",
+            "IMG_0742.jpg"
+        ]
+    },
+    "1304080964": {
+        "images": [
+            "IMG_1029~2.jpg"
+        ]
+    },
+    "1304080968": {
+        "images": [
+            "IMG_0192~2.jpg"
+        ]
+    },
+    "1304080971": {
+        "images": [
+            "432.jpg",
+            "IMG_1887.jpg"
+        ]
+    },
+    "1304080972": {
+        "images": [
+            "IMG_1975~2.jpg",
+            "IMG_1999~2.jpg"
+        ]
+    },
+    "1304216999": {
+        "images": [
+            "IMG_2012.jpg",
+            "IMG_2014~4.jpg"
         ]
     },
     "1304217001": {
         "images": [
             "IMG_0360~3.jpg",
+            "IMG_0362~2.jpg",
             "IMG_0379~2.jpg"
+        ]
+    },
+    "1304439920": {
+        "images": [
+            "IMG_1198~2.jpg",
+            "IMG_1199~2.jpg"
+        ]
+    },
+    "1304439931": {
+        "images": [
+            "IMG_1644~3.jpg"
+        ]
+    },
+    "1304439934": {
+        "images": [
+            "IMG_0015~2.jpg"
+        ]
+    },
+    "1304439963": {
+        "images": [
+            "IMG_0289~2.jpg",
+            "IMG_1547~3.jpg",
+            "IMG_1855~3.jpg"
         ]
     },
     "1304439964": {
         "images": [
-            "IMG_0255~3.jpg"
+            "IMG_0254~2.jpg",
+            "IMG_0255~3.jpg",
+            "IMG_0259~3.jpg",
+            "IMG_1571~2.jpg"
+        ]
+    },
+    "1304439965": {
+        "images": [
+            "IMG_1584~3.jpg"
+        ]
+    },
+    "1304439969": {
+        "images": [
+            "IMG_1496~2.jpg"
+        ]
+    },
+    "1304439971": {
+        "images": [
+            "IMG_1943~3.jpg"
+        ]
+    },
+    "1304439976": {
+        "images": [
+            "IMG_1986~2.jpg"
+        ]
+    },
+    "1304439977": {
+        "images": [
+            "IMG_2016~2.jpg"
         ]
     },
     "1305230699": {},
+    "1307259193": {
+        "images": [
+            "IMG_1172~3.jpg"
+        ]
+    },
+    "1307264371": {
+        "images": [
+            "IMG_1314~2.jpg",
+            "IMG_1357~2.jpg"
+        ]
+    },
     "1307264372": {
         "images": [
-            "IMG_1282~3.jpg"
+            "IMG_1260~3.jpg",
+            "IMG_1282~3.jpg",
+            "IMG_1293~2.jpg",
+            "IMG_1248~2.jpg"
+        ]
+    },
+    "1307264375": {
+        "images": [
+            "IMG_1384~2.jpg",
+            "IMG_1412~3 Copy.JPG"
         ]
     },
     "1307493477": {
         "images": [
+            "IMG_1656~2.jpg",
+            "IMG_1673~2.jpg",
+            "IMG_1680~2.jpg",
             "IMG_1684.jpg"
         ]
     },
     "1307526065": {
         "images": [
-            "IMG_0235~2.jpg"
+            "IMG_0235~2.jpg",
+            "IMG_1614~2.jpg"
+        ]
+    },
+    "1307526066": {
+        "images": [
+            "IMG_1510~3.jpg",
+            "IMG_1512~3.jpg",
+            "IMG_1516~3 Copy.JPG",
+            "IMG_1518~3.jpg",
+            "IMG_1527~2.jpg"
         ]
     },
     "1307526069": {
         "images": [
-            "426.jpg",
-            "IMG_0210~2.jpg"
+            "IMG_0210~2.jpg",
+            "IMG_0220~2.jpg",
+            "IMG_1811~2.jpg"
+        ]
+    },
+    "1307526071": {
+        "images": [
+            "IMG_1821~2.jpg",
+            "IMG_1823~2.jpg"
+        ]
+    },
+    "1307704568": {
+        "images": [
+            "IMG_0027~2.jpg"
         ]
     },
     "1307704573": {
         "images": [
-            "13~~4.jpg",
-            "IMG_0017~2.jpg"
+            "IMG_0017~2.jpg",
+            "IMG_2046~2.jpg"
         ]
     },
     "1307704574": {
         "images": [
-            "IMG_0007~2.jpg"
+            "IMG_0007~2.jpg",
+            "IMG_0008~2.jpg",
+            "IMG_1959~4.jpg"
+        ]
+    },
+    "1307743138": {
+        "images": [
+            "IMG_0752~2.jpg"
+        ]
+    },
+    "1308264124": {
+        "images": [
+            "IMG_1963.jpg"
         ]
     },
     "1308413673": {
         "images": [
             "429.jpg"
+        ]
+    },
+    "1309030776": {
+        "images": [
+            "12.jpg",
+            "436~2.jpg",
+            "436~3.jpg"
+        ]
+    },
+    "1312411199": {
+        "images": [
+            "IMG_1358.jpg",
+            "IMG_1371.jpg"
+        ]
+    },
+    "1316150307": {
+        "images": [
+            "IMG_1870~3.jpg"
         ]
     },
     "1343678316": {
@@ -138,8 +506,17 @@ const meta =  {
         "images": [
             "IMG_1837~3.jpg"
         ]
+    },
+    "1348155895": {
+        "myhint": "post-colonial near that strange dark tower",
+        "images": [
+            "IMG_1416~2",
+            "IMG_1371~2"
+        ]
     }
 }
+
+// TODO: highlight and allow to click non-colonial too
 
 window.french_building_details = meta
 
