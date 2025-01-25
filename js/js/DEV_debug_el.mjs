55ee826aa0d11a67be23f5335d8f4e91c81aa0e8
sbachinin <ssbachinin@gmail.com>
2025-01-25 15:33:09 +0700

    Add debug_el

---
+++ js/js/DEV_debug_el.mjs
@@ -0,0 +1,26 @@
+import { create_element_from_Html } from "../utils.mjs"
+
+let debugel = null
+
+if (window.location.hostname === 'localhost') {
+    debugel = create_element_from_Html(`
+            <div style="position: fixed; z-index: 100;">
+                <div id="debug-zoom"></div>
+            </div>`)
+    document.body.append(debugel)
+}
+
+export const handle_zoom_to_show_in_debug_el = () => {
+    if (debugel === null) return
+
+    const show_zoom = () => {
+        document.querySelector('#debug-zoom').innerHTML = window.dalatmap.getZoom()
+    }
+    window.dalatmap.on('moveend', show_zoom)
+    show_zoom()
+}
+
+export const write_to_debug_el = (text) => {
+    if (debugel === null) return
+    debugel.innerHTML += text
+}
