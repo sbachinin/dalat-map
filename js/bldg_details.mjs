b555414d6e6c1f65c9a85a30330748f183d4b7af
sbachinin <ssbachinin@gmail.com>
2025-03-02 17:01:51 +0700

    Zoom in when to newly selected bldg if zoom was <15.5

--- js/bldg_details.mjs
+++ js/bldg_details.mjs
@@ -3,7 +3,7 @@ import { all_handmade_data } from '../data/static/handmade_data.mjs'
 import { select_bldg, selected_building_id } from './select_building.mjs'
 import { create_panel_thumbs_list } from './panel/panel_thumbs_list.mjs'
 import { update_panel_thumbs_list_size_variables } from './panel/panel_thumbs_list_size_manager.mjs'
-import { coords_are_in_view, get_center_for_bldg_with_offset, push_to_history } from './utils.mjs'
+import { coords_are_in_view, get_map_center_shift, push_to_history } from './utils.mjs'
 import { centroids_etc } from '../data/for_runtime/centroids_etc.mjs'
 
 export const building_has_details = id => {
@@ -60,8 +60,20 @@ export const try_open_building = async (
     await panel.full_size_promise // because panel setsize is async
     const feature_center_arr = centroids_etc[id].centroid
     const feature_screen_xy = window.dalatmap.project(feature_center_arr)
-    if (coords_are_in_view(feature_screen_xy)) return
-    window.dalatmap.easeTo({
-        center: get_center_for_bldg_with_offset(id)
-    })
+    const map_zoom = window.dalatmap.getZoom()
+    if (!coords_are_in_view(feature_screen_xy)
+        || map_zoom < 15.5
+    ) {
+        window.dalatmap.easeTo({
+            /* I used to get center from get_center_for_bldg_with_offset(id)
+             and avoid passing offset
+             but in case of changing zooming this smart offset value was wrong
+             for it was calculated for initial zoom level
+            */
+            center: centroids_etc[id]?.centroid,
+            offset: get_map_center_shift(),
+            zoom: Math.max(15.5, map_zoom),
+            duration: 1600
+        })
+    }
 }
