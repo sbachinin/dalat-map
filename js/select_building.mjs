daee2a757a98fcf2d706d105fcd8939685367b75
sbachinin <ssbachinin@gmail.com>
2025-04-04 09:54:54 +0700

    Fix last selected bldg blinking as selected after switching to highlights and then to another bldg
    By appending an id of a building to id of a newly added "selected" style layer.
    
    Seems that maplibre somehow keeps the old removed "selected" layer with its old filter; then, when layer with the SAME ID is added again, maplibre shows the old version and then WITH DELAY applies a "filter" prop of the newly selected layer

--- js/select_building.mjs
+++ js/select_building.mjs
@@ -21,34 +21,39 @@ export const select_building = newid => {
         const filter = ["==", ["id"], Number(newid)]
 
         if (is_french_building(newid)) {
-            dalatmap.addLayer(
-                deep_merge_objects(
-                    french_buildings_titles,
-                    {
-                        id: SELECTED_STYLE_LAYER_PREFIX + french_buildings_titles.name,
-                        paint: {
-                            "text-halo-color": FRENCH_SELECTED_TITLE_HALO_COLOR,
-                            "text-halo-width": 5,
-                            "text-halo-blur": 0
-                        },
-                        filter
-                    }
+
+                dalatmap.addLayer(
+                    deep_merge_objects(
+                        french_buildings_titles,
+                        {
+                            // bldg id is appended in order to prevent blinking of previously selected building
+                            // (this occured if old bldg was unselected first)
+                            // Appending bldg id fixes it, perhaps because maplibre stops trying to "revive" the old selected layer
+                            id: SELECTED_STYLE_LAYER_PREFIX + french_buildings_titles.name + newid,
+                            paint: {
+                                "text-halo-color": FRENCH_SELECTED_TITLE_HALO_COLOR,
+                                "text-halo-width": 5,
+                                "text-halo-blur": 0
+                            },
+                            filter
+                        }
+                    )
                 )
-            )
-            dalatmap.addLayer(
-                deep_merge_objects(
-                    french_fill_common_props,
-                    {
-                        id: SELECTED_STYLE_LAYER_PREFIX + 'French bldg fill',
-                        "paint": {
-                            "fill-color": FRENCH_SELECTED_FILL_COLOR
-                        },
-                        filter
-                    }
+                dalatmap.addLayer(
+                    deep_merge_objects(
+                        french_fill_common_props,
+                        {
+                            id: SELECTED_STYLE_LAYER_PREFIX + 'French bldg fill' + newid,
+                            "paint": {
+                                "fill-color": FRENCH_SELECTED_FILL_COLOR
+                            },
+                            filter
+                        }
+                    )
                 )
-            )
-            // TODO: can also add layer "selected square at low zoom"
+                // TODO: can also add layer "selected square at low zoom"
         }
+
     }
 
 }
