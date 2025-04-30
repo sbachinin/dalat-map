739ab5d88cee2e9948886c19d04520db9ef505d6
sbachinin <ssbachinin@gmail.com>
2025-04-30 16:53:50 +0700

    Add hue handmade data

---
+++ data/static/hue_handmade_data.mjs
@@ -0,0 +1,103 @@
+export const hue_handmade_data = {
+    "1377444109": { // Hue Industrial College main ancient bldg
+        year: '1924'
+    },
+    "1377444111": {
+        title: 'Workshop',
+        year: '1924'
+    },
+    "1377444112": {
+        title: 'Workshop',
+        year: '1924'
+    },
+    "1377444113": {
+        title: 'Workshop',
+        year: '1924'
+    },
+    "118542146": {
+        title: 'Francis Xavier Church',
+        year: '1911'
+    },
+    "118542754": {
+        // a building in the north of european city, which I have not seen
+        subtitle: 'As of 2025, this building belongs to the grounds of a police department'
+        // photo from gmaps downloaded to Documents/hue
+    },
+
+    "732304130": { // THCS Ng Chí Diểu, very near the center of european part, with rounded "axis"
+        year: 1979
+    },
+
+    "118543157": {
+        // the one that i missed on the southern bank
+        title: 'Center For Vocational Education',
+        // with borrowed image
+    },
+
+    "172763419": {
+        title: 'Imperial city guard headquarters',
+        subtitle: `This building is clearly the one most european in Hue Citadel - and perhaps the least well preserved. It was built in the early 20th century, replacing a building of Royal treasury, and served as the Imperial city guard headquarters in 1936. From 1957 it housed Hue college of fine arts and today it's function is unclear`
+    },
+
+    "531031540": {
+        title: "Tinh Minh Lau villa",
+        subtitle: `Current structure was built in 1927 as a medical clinic and in 1950 rebuilt as a private residence of the king Bao Dai`,
+        links: [
+            {
+                description: 'Amazing 1998 photos of a ruin',
+                url: 'https://www.facebook.com/media/set/?set=a.1716030635284698.1073741847.1714188455468916&type=3&_rdr'
+            }
+        ]
+    },
+
+    1172897446: {
+        title: 'An Dinh Palace',
+        subtitle: `Built in 1919 as a private residence of Emperor Khai Dinh. Being outside Hue's Imperial city and having a distinctly european architectural style, this palace was a very bold statement of Khai Dinh's separation from traditional Confucian imperial power.
+        It's not included in Unesco's "Complex of Hue monuments" and it takes a brief moment to understand why is that.
+        This palace is one of the most opulent structures built in Hue by the Nguyen dynasty.
+        As a piece of architecture it's comparable to Khai Dinh tomb, also a very european structure and also outside the Hue Citadel - but of course european to a lesser degree.
+        The borderline of being Unesco-important is perhaps a bit blurry in Hue. An Dinh Palace indeed must fall short of having an "Outstanding Universal Value", being mostly an exercise in european architecture, however successful it is, and not being sufficiently unique`,
+
+    },
+
+    "Chapel of Nguyen Truong To High School": {
+        subtitle: `As of 2020, this school's campus was a remarkably antiquated place, with some trees and a lot of colonial architecture: 2 extensive classroom buildings with an arcade passage connecting them, and a chapel which I think was one of the most beautiful buildings in Hue. Shortly afterwads all colonial structures were demolished, and in 2025 the campus was newly opened with no trace of anything historical.`
+    },
+
+    "demolished in front of heritage hotel": {
+        // there is a good photo
+        subtitle: "Demolished in 2017. Interestingly enough, it seems that the motive for demolition was simply cutting the maintenance costs rather than development projects (by 2025 nothing new was built on the site). It was too old for profitable commercial use but had to maintain a polished look in order not to ruin the grand view of the adjacent Hue Heritage hotel - a hopeless arrangement indeed."
+    },
+
+    285990348: {
+        title: 'Kien Trung Palace',
+        subtitle: `Boldly eclectic building combining european and vietnamese principles in its overall layout and decoration.
+        Built in 1921-23, used as the main residence of king Bao Dai; destroyed, almost completely, in 1947 during the First Indochina War; restored in 2019-23.
+        A lot of similarities can be found between this structure and An Dinh palace - a more radically europian piece of architecture, built just before Kien Trung.`
+    },
+
+    242940960: {
+        title: 'Thien Dinh Palace',
+        subtitle: `This is the main structure of Khai Dinh tomb complex. It's basically a baroque palace (rather small one) with a vietnamese roof shape and ornaments - a rather audacious fusion that, in my view, works exceptionally well. Its construction took 11 years and ended in 1931. The king Khai Dinh was know for his French tendencies and was ridiculed a "paid employee of the French state", so it's no surprise that this Palace turned out so european, breaking with a long architectural tradition of vietnamese imperial tombs. This building is included in Unesco-protected complex of Hue monuments together with several other imperial tombs. One obvious architectural counterpart of this building is Kien Trung Palace of Hue Imperial city - also a european palace with vietnamese finish, although significantly larger and perhaps less ambitious in its architectural features (Its construction began almost simultaneously with Khai Dinh tomb but was completed much faster, in 1923).`,
+        links: [
+            {
+                description: 'On Google Arts & Culture',
+                url: 'https://artsandculture.google.com/story/bAWhhXL29g5ZIQ?hl=vi'
+            }
+        ]
+    },
+
+    242940961: {
+        title: 'Thien Dinh stele pavilion',
+        // a pavilion in the center of khai dinh
+        subtitile: `This structure houses a large stele - inscribed stone tablet containing a summary of deceased emperor's life. It's a common feature of all royal tombs of Nguyen dynasty, always  standing in the center of the tomb complex. This pavilion, like the quaint palace above it, breaks many architectural traditions of vietnamese imperial tombs by including some european features: arched openings, romanesque columns and octagonal plan (instead of traditionally rectangular). Carvings depict some national motives but their style is rather western - less geometric and more ornate.`
+    },
+
+    bach_dang_street: {
+        // rusty compass says that it's one of the iconic historic streets
+        // one stupid blogger shows a house No. 66 Bach Dang: 
+        // "was formerly a primary school named Saint Piere, built in 1924" (written in 2018)
+    }
+}
+
+
