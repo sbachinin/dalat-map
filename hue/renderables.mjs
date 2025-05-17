fb72e11a6b8a4afe878619e53a2f60d65391539a
sbachinin <ssbachinin@gmail.com>
2025-05-17 17:46:39 +0700

    Add dead buildings renderable to Hue (without features yet)

--- hue/renderables.mjs
+++ hue/renderables.mjs
@@ -1,6 +1,7 @@
 import { city_bulk_border, city_bulk_fill, city_bulk_title } from "../js/common_drawing_layers/city_bulk.mjs";
 import { get_polygon_as_linestring } from "../build/get_polygon_as_linestring.mjs";
 import city_bulk_geometry from "./static_data/city_bulk_geometry.mjs";
+import { dead_buildings_drawing_layers } from "../js/common_drawing_layers/dead_buildings_drawing_layers.mjs";
 
 export const renderables = [
 
@@ -11,5 +12,11 @@ export const renderables = [
             get_polygon_as_linestring(city_bulk_geometry)
         ]),
         style_layers: [city_bulk_fill, city_bulk_border, city_bulk_title]
+    },
+
+    {
+        id: 'Dead_buildings',
+        get_features: () => dead_buildings,
+        style_layers: dead_buildings_drawing_layers
     },
 ]
