c7e7066835cb4f40b539391c779ee7246d0996d4
sbachinin <ssbachinin@gmail.com>
2025-07-08 17:37:03 +0700

    Fix selected building not being really selected when panel isn't opened on pageload due to dev no-open flag

--- js/initialize_panel.mjs
+++ js/initialize_panel.mjs
@@ -9,19 +9,23 @@ export const initialize_panel = () => {
     return new Promise(resolve => {
 
         const should_open_panel = DEV_should_open_panel_on_pageload
+        const selected_bldg_id = get_bldg_id_from_url()
+
+        if (is_feature_selectable(selected_bldg_id)) {
+            // * even if panel isn't expanded, selected building must is still "opened" (just "chosen", silently for now)
+            try_open_building(selected_bldg_id, false, false, should_open_panel)
+        } else if (should_open_panel) {
+            display_highlights()
+        }
 
         if (should_open_panel) {
-            if (is_feature_selectable(get_bldg_id_from_url())) {
-                try_open_building(get_bldg_id_from_url(), false, false)
-            } else {
-                display_highlights()
-            }
             panel.once('begin transition to new size', 'app', resolve)
             panel.expand_button_el.classList.add('inward')
         } else {
             panel.wrapper_element.classList.remove('pristine') // TODO? not "pristine" but "waiting-for-first-expand-transition"
             resolve()
         }
+
         panel.on('content is missing', 'app', display_highlights)
         panel.expand_button_el.classList.remove('hidden')
     })
