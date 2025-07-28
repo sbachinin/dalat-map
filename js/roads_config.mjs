d13e6235f220129592be70ffb3f26976a3ba699c
sbachinin <ssbachinin@gmail.com>
2025-07-28 19:16:11 +0700

    Add roads_config

--- js/roads_config.mjs
+++ js/roads_config.mjs
@@ -1,25 +1,46 @@
 export const roads_hierarchy = [
-    // Major highways
-    "motorway",
-    "motorway_link",
-    "trunk",
-    "trunk_link",
+  // Major highways
+  "motorway",
+  "motorway_link",
+  "trunk",
+  "trunk_link",
 
-    // Arterial roads
-    "primary",
-    "primary_link",
-    "secondary",
-    "secondary_link",
-    "tertiary",
-    "tertiary_link",
+  // Arterial roads
+  "primary",
+  "primary_link",
+  "secondary",
+  "secondary_link",
+  "tertiary",
+  "tertiary_link",
 
-    // Local roads
-    "unclassified",
-    "residential",
-    "living_street",
+  // Local roads
+  "unclassified",
+  "residential",
 
-    // Service & tracks
-    "service",
-    "track"
-    // can be continued with some tiny roads....
+  // other smaller roads for vehicles
+  "living_street",
+  "service",
+  "track",
+  "road", // yet unqualified road
+  "busway",
+  "bus_guideway",
+  "raceway",
+  "escape",
+
+  // not for vehicles, to be rendered as dotted lines
+  "footway", "path", "cycleway", "steps", 'pedestrian', 'bridleway', 'corridor'
+]
+/* 
+  ****
+  'construction' type is not included. It's temporary and I tend not to include temporary things
+*/
+
+
+export const roads_config = [
+  { 0: 'motorway' },
+  { 13.5: 'secondary' },
+  { 14.2: 'tertiary' },
+  { 15: 'unclassified' },
+  { 15.5: 'living_street' },
+  { 15.5: 'footway' }
 ]
