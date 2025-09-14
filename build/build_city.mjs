6875a563a88c42a0daafb74a720ed94cb0933195
sbachinin <ssbachinin@gmail.com>
2025-09-14 09:28:52 +0700

    Update validate_images. Run it from build_city, if flag is true
    Run them for a given city (instead of Dalat),
    
    extract rejected_imgs to a file

--- build/build_city.mjs
+++ build/build_city.mjs
@@ -1,21 +1,23 @@
 import { execSync } from 'child_process';
 import { parse_args } from "./build_utils.mjs"
-
-const args = parse_args()
+import { validate_images } from '../chores/validate_images.mjs';
 
 // config_to_adjust_manually
 const defaults = {
     city: 'malacca',
-    generate_tiles: 1,
+    generate_tiles: 0,
     skip_osm_download: 1,
     process_images: 0,
-    purge_old_images: 1
+    validate_images: 1,
+    purge_old_images: 0
 }
 
+const args = parse_args()
 const city = args.city || defaults.city
 const generate_tiles = args.generate_tiles || defaults.generate_tiles
 const skip_osm_download = defaults.skip_osm_download
 const process_images = args.process_images || defaults.process_images
+const validate_images = args.validate_images || defaults.validate_images
 const purge_old_images = args.purge_old_images || defaults.purge_old_images
 
 console.log('Building city:', city.toUpperCase())
@@ -30,6 +32,10 @@ if (process_images) {
         `cd ./images && node ./process_images.mjs city=${city} ${purge_old_images ? 'force' : ''}`,
         { stdio: 'inherit' }
     )
+}
+
+if (validate_images) {
+    await validate_images(city)
 }
 
 if (generate_tiles) {
--- chores/validate_images_data.mjs
+++ chores/validate_images_data.mjs
@@ -1,132 +1,60 @@
 /* 
-  This answers the questions:
-  1) Haven't I failed to use all the available images in buildings' data?
-  Copy them from "large" folder to "missing" folder in order to drop them to browser
-  2) Don't I use some nonexistent filenames in buildings data? ("broken links")
+  This checks that:
+  1) I didn't forget to add some images to the buildings
+  2) I don't use any nonexistent filenames in buildings data
 */
 
 import fs from 'fs'
 import path from 'path'
-import { all_handmade_data } from '../dalat/static_data/handmade_data.mjs'
 
-const rejected_imgs = [
-    "IMG_9858~3.jpg", 'IMG_0475~2.jpg', 'IMG_0613~2.jpg',
-    'IMG_0708~4.jpg', 'IMG_0724.jpg', 'IMG_0745~5.jpg',
-    'IMG_0758~4.jpg', 'IMG_0808.jpg', 'IMG_1468~5.jpg',
+export const validate_images = async (cityname) => {
 
-    "IMG_9516~2.jpg",
+    const large_img_dir = path.resolve(`../cities_images/${cityname}/dist/large`)
+    const city_root_dir = `../${cityname}`
+    const fids_to_img_names = (await import(city_root_dir + '/static_data/fids_to_img_names.mjs')).fids_to_img_names
 
-    'IMG_2199.jpg', 'IMG_2259.jpg',
-    'IMG_2342~2.jpg', 'IMG_2544~2.jpg', 'IMG_2753.jpg',
-    'IMG_3017~2.jpg', 'IMG_3019~2.jpg', 'IMG_3063.JPG',
-    'IMG_3195.jpg', 'IMG_3364~2.jpg', 'IMG_3444~2.jpg',
-    'IMG_3853~2.jpg', 'IMG_3860~3.jpg', 'IMG_4057~2.jpg',
-    'IMG_4169~2.jpg', 'IMG_4682~2.jpg', 'IMG_4701~2.jpg',
-    'IMG_4957~3.jpg', 'IMG_5543.jpg',
-    "IMG_8514~3.jpg",
-    'IMG_6489~3.jpg', 'IMG_7070~3.jpg',
-    'IMG_7339~2.jpg',
-    "IMG_9487~2.jpg",
-    'IMG_7972.jpg',
-    'IMG_8275~4.jpg', 'IMG_8387~3.jpg', 'IMG_8412~3.jpg',
-    'IMG_8531~3.jpg', 'IMG_8548~2.jpg', 'IMG_8558 Copy~2.JPG',
-    'IMG_8580~5.jpg', 'IMG_8848~3.jpg', 'IMG_8854~2.jpg',
-    'IMG_8872~2.jpg', 'IMG_9115~2.jpg', 'IMG_9153.jpg',
+    let rejected_imgs = []
+    try {
+        rejected_imgs = (await import(city_root_dir + '/static_data/rejected_imgs.mjs')).rejected_imgs
+    } catch (e) { }
 
-    "IMG_1358.jpg",
-
-    "IMG_2069.jpg",
-    'IMG_9564.jpg',
-    'IMG_9651~2.jpg', 'IMG_9668~2.jpg',
-    'quaint 3storey.jpg', 'IMG_1029~2.jpg',
-    "IMG_9681~2.jpg", "IMG_2341.jpg",
-    "IMG_2243~2.jpg", "IMG_2840.jpg", "IMG_2258~2.jpg",
-    "IMG_2600.jpg", "IMG_7572~5.jpg", "IMG_8683~2.jpg",
-    "IMG_8675~4.jpg", "IMG_0008~2.jpg",
-    "IMG_7957~3.jpg",
-    "IMG_2355.jpg",
-    "IMG_9547~2.jpg",
-    "IMG_0615~2.jpg",
-    "IMG_9748~2.jpg",
-
-    "IMG_4113~2.jpg",
-    "IMG_6958 Copy~2.JPG",
-    "IMG_4293~2.jpg",
-    "IMG_6323~5.jpg",
-    "IMG_2134~3.jpg",
-    "IMG_6407~2.jpg",
-    "IMG_8837~2.jpg",
-    "IMG_4716.jpg",
-    "IMG_0082~2.JPG",
-    "IMG_2014~4.jpg",
-    "IMG_8150~2.jpg",
-    "IMG_1999.jpg",
-    "IMG_6464~2.jpg",
-    "IMG_3256.jpg",
-    "IMG_0230~3.jpg",
-    "IMG_4399.jpg",
-    "IMG_0492~3.jpg",
-    "IMG_8364~2.jpg",
-    "IMG_4050~2.jpg",
-    "436~2.jpg",
-    "IMG_3192~3.jpg",
-    "IMG_4053.jpg",
-    "IMG_0489~3.jpg",
-    "IMG_7687~3.jpg",
-    "IMG_1563~5.jpg",
-    "IMG_9137~2.jpg",
-    "IMG_3108~3.jpg",
-    "IMG_1172~3.jpg",
-    "IMG_9608~4.jpg",
-    "IMG_9319~3.jpg",
-
-]
-
-const largeImgDir = path.resolve('./dalat-map-images/large')
-const missing_imgs_dir = path.resolve('./dalat-map-images/missing')
-
-
-export const validate_images_data = () => {
-
-    // make or empty the "missing" folder
-    if (!fs.existsSync(missing_imgs_dir)) {
-        fs.mkdirSync(missing_imgs_dir)
-    } else {
-        fs.readdirSync(missing_imgs_dir).forEach(file => {
-            fs.unlinkSync(path.join(missing_imgs_dir, file))
-        })
-    }
-
-
-
-    const file_imgs = fs.readdirSync(largeImgDir)
-    const json_imgs = Object.values(all_handmade_data)
-        .flatMap(feature => feature.images || [])
-
-
-
-
-
+    const files_imgs_names = fs.readdirSync(large_img_dir)
+    const all_buildings_imgs_names = Object.values(fids_to_img_names)
+        .flatMap(arr => arr)
 
 
 
     // Check if I failed to add all available images to handmade data
-    // copy them to "missing" folder
-    const json_set = new Set(json_imgs)
-    const missing_in_json = file_imgs.filter(img => {
-        return !rejected_imgs.includes(img)
+    // copy them to "missing" folder to drop them later to their buildings
+    const missing_imgs_dir = path.resolve(`../cities_images/${cityname}/missing`)
+    const json_set = new Set(all_buildings_imgs_names)
+    const orphan_imgs_filenames = files_imgs_names.filter(img => {
+        return !rejected_imgs.includes(img) // some imgs are omitted intentionally => don't yell
             && !json_set.has(img)
     })
-    if (missing_in_json.length === 0) {
+    if (orphan_imgs_filenames.length === 0) {
         console.log('All generated images are used in handmade data (excluding those explicitly rejected)')
+        // rm missing dir
+        const missing_imgs_dir = path.resolve(`../cities_images/${cityname}/missing`)
+        if (fs.existsSync(missing_imgs_dir)) {
+            fs.rmSync(missing_imgs_dir, { recursive: true, force: true })
+        }
     } else {
-        missing_in_json.forEach(img => {
+        // make sure "missing" folder exists and is empty
+        if (fs.existsSync(missing_imgs_dir)) {
+            fs.readdirSync(missing_imgs_dir).forEach(file => {
+                fs.unlinkSync(path.join(missing_imgs_dir, file))
+            })
+        } else {
+            fs.mkdirSync(missing_imgs_dir)
+        }
+
+        orphan_imgs_filenames.forEach(img => {
             console.log(img + ' is missing in handmade data and was copied to "missing" folder')
-            const oldPath = path.join(largeImgDir, img)
+            const oldPath = path.join(large_img_dir, img)
             const missing_path = path.join(missing_imgs_dir, img)
             fs.copyFileSync(oldPath, missing_path)
         })
-        process.exit(1)
     }
 
 
@@ -134,8 +62,8 @@ export const validate_images_data = () => {
 
 
     // complain if handmade data contains nonexistent filenames
-    const file_set = new Set(file_imgs)
-    const missing_in_files = json_imgs.filter(img => !file_set.has(img))
+    const file_set = new Set(files_imgs_names)
+    const missing_in_files = all_buildings_imgs_names.filter(img => !file_set.has(img))
     if (missing_in_files.length === 0) {
         console.log('All handmade data images are present in files')
     } else {
