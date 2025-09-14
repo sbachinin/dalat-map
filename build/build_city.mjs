9136503d7435128a7f7c4c89263cefd4dddd0a18
sbachinin <ssbachinin@gmail.com>
2025-09-14 09:32:03 +0700

    Move validate_images to build/

--- build/build_city.mjs
+++ build/build_city.mjs
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process';
 import { parse_args } from "./build_utils.mjs"
-import { validate_images } from '../chores/validate_images.mjs';
+import { validate_images } from './validate_images.mjs'
 
 // config_to_adjust_manually
 const defaults = {
@@ -8,7 +8,7 @@ const defaults = {
     generate_tiles: 0,
     skip_osm_download: 1,
     process_images: 0,
-    validate_images: 1,
+    shall_validate_images: 1,
     purge_old_images: 0
 }
 
@@ -17,7 +17,7 @@ const city = args.city || defaults.city
 const generate_tiles = args.generate_tiles || defaults.generate_tiles
 const skip_osm_download = defaults.skip_osm_download
 const process_images = args.process_images || defaults.process_images
-const validate_images = args.validate_images || defaults.validate_images
+const shall_validate_images = args.shall_validate_images || defaults.shall_validate_images
 const purge_old_images = args.purge_old_images || defaults.purge_old_images
 
 console.log('Building city:', city.toUpperCase())
@@ -34,7 +34,7 @@ if (process_images) {
     )
 }
 
-if (validate_images) {
+if (shall_validate_images) {
     await validate_images(city)
 }
 
