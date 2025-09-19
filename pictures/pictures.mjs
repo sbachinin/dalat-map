b7990b21332f919716cc41b816fbc32273440cb4
sbachinin <ssbachinin@gmail.com>
2025-09-19 15:41:01 +0700

    Display unassigned images in pictures viewer

--- pictures/pictures.mjs
+++ pictures/pictures.mjs
@@ -3,14 +3,18 @@ const cityname = params.get('city')
 if (!cityname) {
     document.getElementById('output').textContent = '(no city provided)'
 } else {
-    const modulePath = `../${cityname}/static_data/fids_to_img_names.mjs`
-    import(modulePath).then(mod => {
-        const ftin = mod.fids_to_img_names
-        display(cityname, ftin)
+    const fids_path = `../${cityname}/static_data/fids_to_img_names.mjs`
+    const unassigned_images_path = `../${cityname}/static_data/unassigned_images.mjs`
+    Promise.allSettled([
+        import(fids_path),
+        import(unassigned_images_path)
+    ]).then(res => {
+        display_bldgs_images(cityname, res[0].value.fids_to_img_names)
+        display_unassigned_images(cityname, res[1].value?.unassigned_images)
     })
 }
 
-function display(cityname, ftin) {
+function display_bldgs_images(cityname, ftin) {
     for (const [fid, images_basenames] of Object.entries(ftin)) {
         const section = document.createElement('div')
         section.classList.add('one-building')
@@ -31,4 +35,20 @@ function display(cityname, ftin) {
 
         document.getElementById('output').appendChild(section)
     }
+}
+
+function display_unassigned_images(cityname, unassigned_images) {
+    if (!unassigned_images) {
+        return
+    }
+    const section = document.createElement('div')
+    section.classList.add('unassigned-images')
+    section.innerHTML = '<h2>Unassigned images</h2>'
+    unassigned_images.forEach(basename => {
+        const img = document.createElement('img')
+        img.src = `../cities_images/${cityname}/dist/thumbs/${basename}.jpg`
+        img.loading = "lazy"
+        section.appendChild(img)
+    })
+    document.getElementById('output').appendChild(section)
 }
