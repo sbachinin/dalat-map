17adf4d043c4c83ee897e3a264b9c1957b8b59cd
sbachinin <ssbachinin@gmail.com>
2025-09-20 14:02:44 +0700

    Write basenames (not full names) to highlights list when retreiving the order from google photos
    (Update instruction for retreiving of highlights)
    
    Write each name on a new line, for readable diff

--- chores/howto_retrieve_order_of_highlights.js
+++ chores/howto_retrieve_order_of_highlights.js
@@ -14,11 +14,22 @@ setInterval(() => {
     ).find(el => el.offsetWidth > 0 && el.offsetHeight > 0)
     const filename = visibleFilename.innerText
 
-    if (!names.includes(filename)) names.push(filename)
+    const basename = filename.substring(0, filename.lastIndexOf('.'))
+
+    if (!names.includes(basename)) names.push(basename)
     document.querySelectorAll('[role="button"][aria-label="View next photo"]')[0].click()
+    console.log('Number of images:', names.length)
 }, 1000)
 
+// after the last image was reached:
+
+copy(`[
+    '${names.join(`',\n'`)}'
+]`)
 
 // Last image can be missing. Add it.
+
+// Check the number of images
+// names.length must be === Album size
 
 // save to highlights_images_list.mjs
