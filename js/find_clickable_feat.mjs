77e9ab101171c9109c1ba1c83c284a05d955da15
sbachinin <ssbachinin@gmail.com>
2025-10-19 14:36:40 +0700

    Add support for Point features (circles) when searching for a selectable feature around the missed click (tap)
    Before this, tap on a circle had to be precise. If it was very near the circle, the circle (belonging to a style layer other than "fill") was filtered out as an impossible feat.

--- js/find_clickable_feat.mjs
+++ js/find_clickable_feat.mjs
@@ -1,7 +1,7 @@
 import { is_feature_selectable } from "./utils/does_feature_have_details.mjs"
 import { is_mouse_device } from "./utils/frontend_utils.mjs"
 
-const clickable_extra_thickness = is_mouse_device ? 7 : 20 // smaller mistake tolerance for mouse because it's harder to miss with mouse
+const click_tolerance = is_mouse_device ? 5 : 15 // smaller mistake tolerance for mouse because it's harder to miss with mouse
 
 export const find_clickable_feat = (click_point) => {
 
@@ -17,8 +17,8 @@ export const find_clickable_feat = (click_point) => {
     }
 
     const bbox = [
-        [click_point.x - clickable_extra_thickness, click_point.y - clickable_extra_thickness],
-        [click_point.x + clickable_extra_thickness, click_point.y + clickable_extra_thickness]
+        [click_point.x - click_tolerance, click_point.y - click_tolerance],
+        [click_point.x + click_tolerance, click_point.y + click_tolerance]
     ]
     const rfs_around_point = window.dalatmap.queryRenderedFeatures(bbox)
 
@@ -28,16 +28,32 @@ export const find_clickable_feat = (click_point) => {
     }
 
     const geo_point = window.dalatmap.unproject(click_point)
-    const arr_point = [geo_point.lng, geo_point.lat]
+    const click_point_as_arr = [geo_point.lng, geo_point.lat]
 
     let smallest_distance_to_click = Infinity
     let nearest_feat = null
     clickable_feats_around_point
-        .filter(f => f.layer.type === 'fill')
+        .filter(f => f.layer.type === 'fill'
+            || f.layer.type === 'symbol'
+        )
         .forEach(f => {
-            const pline = globalThis.turf.polygonToLine(f)
-            const nearest_point = globalThis.turf.nearestPointOnLine(pline, arr_point)
-            const d = globalThis.turf.distance(arr_point, nearest_point, { units: 'kilometers' })
+            let feat_nearest_point = null
+
+            if (f.geometry.type === 'Point') {
+                feat_nearest_point = f
+            } else if (f.geometry.type === 'Polygon' || f.geometry.type === 'MultiPolygon') {
+                const pline = globalThis.turf.polygonToLine(f)
+                feat_nearest_point = globalThis.turf.nearestPointOnLine(pline, click_point_as_arr)
+            } else {
+                return
+            }
+
+            const d = globalThis.turf.distance(
+                click_point_as_arr,
+                feat_nearest_point,
+                { units: 'kilometers' }
+            )
+
             if (d < smallest_distance_to_click) {
                 smallest_distance_to_click = d
                 nearest_feat = f
