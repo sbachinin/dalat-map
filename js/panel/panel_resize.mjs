abd21f8efefb70c0675b5427cc96c0cc7a2d292b
sbachinin <ssbachinin@gmail.com>
2025-11-07 16:07:47 +0700

    Use window-was-just-resized root html attribute to disable transitions on panel,
    instead of setting notransition class

--- js/panel/panel_resize.mjs
+++ js/panel/panel_resize.mjs
@@ -3,13 +3,15 @@ import { panel } from './panel.mjs'
 export const adjust_panel_on_resize = () => {
 
     // Prevents untidy movements when orientation changes on mobile while panel is open
-    panel.wrapper_element.classList.add('notransition')
-    
+    document.documentElement.setAttribute('window-was-just-resized', true)
+
     panel.content?.update_size()
     panel.wrapper_element.scrollTop = 0
     panel.wrapper_element.scrollLeft = 0
     panel.cache_content_breadth()
 
     panel.set_size(panel.is_at_least_partially_expanded() ? panel.content_breadth : 0)
-    setTimeout(() => {panel.wrapper_element.classList.remove('notransition')}, 0)
+    setTimeout(() => {
+        document.documentElement.setAttribute('window-was-just-resized', false)
+    }, 0)
 }
